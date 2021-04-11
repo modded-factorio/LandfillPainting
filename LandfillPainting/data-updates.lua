b0bd334f60ab4a16ad8577035c4ef0d1a3a545ac
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-04-11 18:58:33 +1200

    Error if boblibrary isn't enabled #10

--- LandfillPainting/data-updates.lua
+++ LandfillPainting/data-updates.lua
@@ -15,5 +15,17 @@ for _,v in pairs(data.raw.tile) do
   end
 end
 
-bobmods.lib.tech.remove_recipe_unlock('water-washing-2', 'solid-mud-landfill')
-bobmods.lib.tech.add_recipe_unlock('water-washing-1', 'solid-mud-landfill')
+local technology = data.raw.technology['water-washing-2']
+
+if technology and technology.effects then
+  for i, effect in pairs(technology.effects) do
+    if effect.type == 'unlock-recipe' and effect.recipe == 'solid-mud-landfill' then
+      table.remove(technology.effects, i)
+    end
+  end
+end
+
+technology = data.raw.technology['water-washing-1']
+if technology then
+  table.insert(technology.effects, {type = 'unlock-recipe', recipe = 'solid-mud-landfill'})
+end
