31ae9bd9eaf5d1165063695c130b5c6abf1c7e0a
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-06-10 22:16:56 +1200

    Updating placement rules

--- LandfillPainting/data.lua
+++ LandfillPainting/data.lua
@@ -1,7 +1,7 @@
 require "util"
 local tile_sounds = require("__base__/prototypes/tile/tile-sounds")
 
--- Define the names and tile type lanfill we are adding
+-- Define the names and tile type landfill we are adding
 local item_tile_map = {
   ["landfill-dry-dirt"] = "dry-dirt",
   ["landfill-dirt"] = "dirt-4",
@@ -11,33 +11,31 @@ local item_tile_map = {
   ["landfill"] = "landfill",
 }
 
-local tile_item_map = {}
 local overwritable_tiles = data.raw.item["landfill"].place_as_tile.tile_condition
 
-for tile_name, item_name in pairs({
-  ["dry-dirt"] = "landfill-dry-dirt",
-  ["dirt-1"] = "landfill-dirt",
-  ["dirt-2"] = "landfill-dirt",
-  ["dirt-3"] = "landfill-dirt",
-  ["dirt-4"] = "landfill-dirt",
-  ["dirt-5"] = "landfill-dirt",
-  ["dirt-6"] = "landfill-dirt",
-  ["dirt-7"] = "landfill-dirt",
-  ["grass-1"] = "landfill-grass",
-  ["grass-2"] = "landfill-grass",
-  ["grass-3"] = "landfill-grass",
-  ["grass-4"] = "landfill-grass",
-  ["landfill"] = "landfill",
-  ["red-desert-0"] = "landfill-red-desert",
-  ["red-desert-1"] = "landfill-red-desert",
-  ["red-desert-2"] = "landfill-red-desert",
-  ["red-desert-3"] = "landfill-red-desert",
-  ["sand-1"] = "landfill-sand",
-  ["sand-2"] = "landfill-sand",
-  ["sand-3"] = "landfill-sand",
+for _, tile_name in pairs({
+  "dry-dirt",
+  "dirt-1",
+  "dirt-2",
+  "dirt-3",
+  "dirt-4",
+  "dirt-5",
+  "dirt-6",
+  "dirt-7",
+  "grass-1",
+  "grass-2",
+  "grass-3",
+  "grass-4",
+  "landfill",
+  "red-desert-0",
+  "red-desert-1",
+  "red-desert-2",
+  "red-desert-3",
+  "sand-1",
+  "sand-2",
+  "sand-3",
 }) do
   if data.raw.tile[tile_name] then
-    tile_item_map[tile_name] = item_name
     table.insert(overwritable_tiles, tile_name)
   end
 end
@@ -86,36 +84,38 @@ end
 
 -- Add new items and recipes for each terrain type
 for item_name, tile_name in pairs(item_tile_map) do
-  local item = {
-    type = "item",
-    name = item_name,
-    localised_description = { "item-description.landfill" },
-    icon = "__LandfillPainting__/graphics/icons/" .. item_name .. ".png",
-    icon_size = 64,
-    subgroup = "terrain",
-    order = "c[landfill]-a[" .. item_name .. "]",
-    stack_size = 100,
-    place_as_tile =
-    {
-      result = tile_name,
-      condition_size = 1,
-      --condition = { layers = { ground_tile = true }},
-      condition = { layers = {} },
-      -- Enable all types of terrain that LandfillPainter allows the player to create to replace each other
-      tile_condition = table.deepcopy(overwritable_tiles),
-    },
-  }
-  local recipe = util.table.deepcopy(baserecipe)
-  recipe.name = item_name
-  recipe.results = {{ type = "item", name = item_name, amount = 1 }}
+  if data.raw.tile[tile_name] then
+    local item = {
+      type = "item",
+      name = item_name,
+      localised_description = { "item-description.landfill" },
+      icon = "__LandfillPainting__/graphics/icons/" .. item_name .. ".png",
+      icon_size = 64,
+      subgroup = "terrain",
+      order = "c[landfill]-a[" .. item_name .. "]",
+      stack_size = 100,
+      place_as_tile =
+      {
+        result = tile_name,
+        condition_size = 1,
+        condition = { layers = {} },
+        -- Enable all types of terrain that LandfillPainter allows the player to create to replace each other
+        tile_condition = table.deepcopy(overwritable_tiles),
+      },
+    }
+    local recipe = util.table.deepcopy(baserecipe)
+    recipe.name = item_name
+    recipe.results = {{ type = "item", name = item_name, amount = 1 }}
 
-  data:extend({item, recipe})
-  add_recipe_unlock(item_name)
+    data:extend({item, recipe})
+    add_recipe_unlock(item_name)
+  end
 end
 
-for tile_name, item_name in pairs(tile_item_map) do
+for item_name, tile_name in pairs(item_tile_map) do
   local tile = data.raw.tile[tile_name]
-  tile.can_be_part_of_blueprint = nil
-  tile.is_foundation = true
-  tile.build_sound = tile_sounds.building.landfill
+  if tile then
+    tile.can_be_part_of_blueprint = nil
+    tile.build_sound = tile_sounds.building.landfill
+  end
 end
